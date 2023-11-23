55d8773d4aa021420996728e5dab4f64b2d42f8e
Franklyn Vasquez <franklynvasquez24@gmail.com>
2023-11-22 21:28:56 -0500

    Added index, findex

---
+++ src/stdcol_index.cpp
@@ -0,0 +1,22 @@
+#include <stdcol_index>
+
+using namespace stdcol;
+
+index::index() : value(0) {}
+
+index::index(const int_type& value) : value(value) {}
+
+index::int_type& index::operator=(const int_type& value) {
+    this->value = value;
+    return this->value;
+}
+
+index::operator stdcol::index::int_type&() { return value; }
+
+index::operator const stdcol::index::int_type&() const { return value; }
+
+findex::findex() : index(not_found) {}
+
+findex::findex(const int_type& value) : index(value) {}
+
+bool findex::found() const { return value != not_found; }
