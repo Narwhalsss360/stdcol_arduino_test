e57ca6a8d3e1044d068c9c29240c98bb1c94bc90
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-02-13 22:43:22 -0500

    finished arduinofy

--- tools/arduinofy.py
+++ tools/arduinofy.py
@@ -1,7 +1,7 @@
 '''Script for arduinofy repository '''
 
-from os import path, remove
-from shutil import move
+from os import path, remove, rmdir, listdir
+from shutil import move, rmtree
 from sys import argv
 
 metadata_files_src_dst = {
@@ -13,6 +13,13 @@ delete_folders = [
     'include',
     'meta',
     'test'
+]
+
+delete_files = [
+    'stdcol.sln',
+    'stdcol.vcxitems',
+    'stdcol.vcxitems.filters',
+    'makefile'
 ]
 
 if not path.exists('.git'):
@@ -33,11 +40,22 @@ for file, (src, dst) in metadata_files_src_dst.items():
         continue
     print(f'{file} : {src} {dst}')
 
+src_dir = 'include'
+dst_dir = 'src'
+for file in listdir('include'):
+    move(f'{src_dir}/{file}', f'{dst_dir}/{file}')
+
+for file in delete_files:
+    if not path.isfile(file):
+        print(f'{file} not found')
+        continue
+    remove(file)
+
 for folder in delete_folders:
     if not path.isdir(folder):
         print(f'{folder} was not found')
         continue
-    remove(folder)
+    rmtree(folder)
 
 print('To restore, run git restore ./')
 #remove(argv[0])
